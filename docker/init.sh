97f3affc69da21ff21bcd3b7b3f0c00cb809b992
Sam Millman <sam.millman@googlemail.com>
2025-02-14 11:06:56 +0000

    WIP

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-nordvpn_token = `cat /run/secrets/nordvpn_token`;
-/etc/init.d/nordvpn start && sleep 5 && nordvpn login --token "$nordvpn_token"; service transmission-daemon start
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    printf '%s' "$var"
+}
+
+nordvpn_token=$(</run/secrets/nordvpn_token)
+nordvpn_token=$(trim "$nordvpn_token")
+
+/etc/init.d/nordvpn start && sleep 5 && nordvpn login --token $nordvpn_token && service transmission-daemon start
+
